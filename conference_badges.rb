cdd4dfaa11d6e45ad7a6bfd9443a5152b2718854
Cindy Vo <yendai.vo@hotmail.com>
2018-07-11 17:57:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,5 +8,5 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-  array.each_with_index! {|val, index| puts "Hello, #{val}! You'll be assigned to room #{(index + 1)}!"}
+  array.each_with_index {|val, index| "Hello, #{val}! You'll be assigned to room #{(index + 1)}!"}
 end
